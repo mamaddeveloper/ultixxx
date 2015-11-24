5c85d0b04705f85572faffcd2748568d177897d6
ArmanIrbot <tgclibot@gmail.com>
2015-11-24 22:53:02 +0330

    Create ult.lua

---
+++ plugins/ult.lua
@@ -0,0 +1,14 @@
+do
+
+function run(msg, matches)
+  return " You Have Choosen UltraKing🔱\nIn Ultra King You Have To Join This Group🔽\nhttps://telegram.me/joinchat/B4ghtgIvpYlMkBZTG57OhQ\nYou Have To Pay2000 Each Month"
+end
+return {
+  description = "Invite bot into a group chat", 
+  usage = "!join [invite link]",
+  patterns = {
+    "^/ultra$",
+  },
+  run = run
+}
+end
