98aaa4c0398857e650b2a0fdcf2fd3a9e3b0781a
ArmanIrbot <tgclibot@gmail.com>
2015-11-24 22:11:50 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -42,40 +42,40 @@ end
 
 function msg_valid(msg)
   -- Don't process outgoing messages
-  if msg.out then
-    print('\27[36mNot valid: msg from us\27[39m')
-    return true
-  end
+  --if msg.out then
+   -- print('\27[36mNot valid: msg from us\27[39m')
+   -- return false
+  --end
 
   -- Before bot was started
   if msg.date < now then
     print('\27[36mNot valid: old msg\27[39m')
-    return true
+    return false
   end
 
   if msg.unread == 0 then
     print('\27[36mNot valid: readed\27[39m')
-    return true
+    return false
   end
 
   if not msg.to.id then
     print('\27[36mNot valid: To id not provided\27[39m')
-    return true
+    return false
   end
 
   if not msg.from.id then
     print('\27[36mNot valid: From id not provided\27[39m')
-    return true
+    return false
   end
 
-  if msg.from.id == 126362038 then
-    print('\27[36mNot valid: Msg from our id\27[39m')
-    return true
-  end
+  --if msg.from.id == our_id then
+    --print('\27[36mNot valid: Msg from our id\27[39m')
+   -- return false
+  --end
 
   if msg.to.type == 'encr_chat' then
     print('\27[36mNot valid: Encrypted chat\27[39m')
-    return true
+    return false
   end
 
   if msg.from.id == 777000 then
